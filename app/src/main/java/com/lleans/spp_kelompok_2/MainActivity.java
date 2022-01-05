040d4759eb8052b9b842b4d017c719f79c2a23b8
salsabiladiah7 <salsabila_diah_28rpl@student.smktelkom-mlg.sch.id>
2022-01-05 11:27:38 +0700

    Commit List Kelas 1

--- app/src/main/java/com/lleans/spp_kelompok_2/MainActivity.java
+++ app/src/main/java/com/lleans/spp_kelompok_2/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
+        setContentView(R.layout.homepage_petugas);
     }
 }
