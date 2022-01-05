8767dfbf83deafd937a72c531a25566c2d6e58d7
salsabiladiah7 <salsabila_diah_28rpl@student.smktelkom-mlg.sch.id>
2022-01-05 10:48:28 +0700

    Commit Login 1

--- app/src/main/java/com/lleans/spp_kelompok_2/MainActivity.java
+++ app/src/main/java/com/lleans/spp_kelompok_2/MainActivity.java
@@ -1,14 +1,14 @@
 package com.lleans.spp_kelompok_2;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login);
     }
 }
