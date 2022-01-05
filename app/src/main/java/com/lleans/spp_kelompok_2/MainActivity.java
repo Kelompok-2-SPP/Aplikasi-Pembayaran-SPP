a029a75896ef6e35349cee3cd8647d41194d6cf6
llean's <ahmadunnail@gmail.com>
2022-01-05 12:23:52 +0700

    add nav example

--- app/src/main/java/com/lleans/spp_kelompok_2/MainActivity.java
+++ app/src/main/java/com/lleans/spp_kelompok_2/MainActivity.java
@@ -1,14 +1,30 @@
 package com.lleans.spp_kelompok_2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button tes;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
+        setContentView(R.layout.activity_main);
+
+        tes = (Button) findViewById(R.id.buttonTest);
+        tes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent inte = new Intent(MainActivity.this, LauncherFragment.class);
+                MainActivity.this.startActivity(inte);
+            }
+        });
+
     }
 }
