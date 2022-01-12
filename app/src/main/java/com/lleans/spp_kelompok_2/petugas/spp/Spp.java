8dd94eb7ecec718c5be4629e3a3a175fe75ffc9f
RakhaGalih <54633534+RakhaGalih@users.noreply.github.com>
2022-01-12 08:16:48 +0700

    spp

--- app/src/main/java/com/lleans/spp_kelompok_2/petugas/spp/Spp.java
+++ app/src/main/java/com/lleans/spp_kelompok_2/petugas/spp/Spp.java
@@ -2,7 +2,11 @@ package com.lleans.spp_kelompok_2.petugas.spp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -25,5 +29,14 @@ public class Spp extends Fragment {
         // Inflate the layout for this fragment
         binding = SppPetugasBinding.inflate(inflater, container, false);
         return binding.getRoot();
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Contoh navigation
+        final NavController nav = Navigation.findNavController(view);
+        // Cari id navigation di nav graph
+        binding.petugasLanjut.setOnClickListener(v -> nav.navigate(R.id.action_spp_petugas_to_rincianSpp_petugas));
     }
 }
