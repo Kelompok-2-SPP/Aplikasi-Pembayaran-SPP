07b1740844141187757eab6ca79e4a1e460d40d5
safrinauriya <60589670+safrinauriya@users.noreply.github.com>
2022-01-18 10:52:41 +0700

    + kelas

--- app/src/main/java/com/lleans/spp_kelompok_2/ui/main/petugas/kelas/Kelas.java
+++ app/src/main/java/com/lleans/spp_kelompok_2/ui/main/petugas/kelas/Kelas.java
@@ -1,15 +1,17 @@
 package com.lleans.spp_kelompok_2.ui.main.petugas.kelas;
 
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import com.lleans.spp_kelompok_2.R;
 import com.lleans.spp_kelompok_2.databinding.KelasPetugasBinding;
 
 public class Kelas extends Fragment {
@@ -23,6 +25,8 @@ public class Kelas extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        final NavController nav = Navigation.findNavController(view);
+        binding.btnTambahKelas.setOnClickListener(v -> nav.navigate(R.id.action_homepage_petugas_to_kelas_petugas));
     }
 
     @Override
