3afb86e910764b64ac03a8a3e7f409a15c057bcd
llean's <ahmadunnail@gmail.com>
2022-01-23 20:13:36 +0700

    lupa

--- app/src/main/java/com/lleans/spp_kelompok_2/ui/main/petugas/kelas/EditKelas.java
+++ app/src/main/java/com/lleans/spp_kelompok_2/ui/main/petugas/kelas/EditKelas.java
@@ -2,6 +2,7 @@ package com.lleans.spp_kelompok_2.ui.main.petugas.kelas;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,58 +10,29 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.lleans.spp_kelompok_2.R;
+import com.lleans.spp_kelompok_2.databinding.KelasPetugasBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link EditKelas#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class EditKelas extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private KelasPetugasBinding binding;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public EditKelas() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment EditKelas.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static EditKelas newInstance(String param1, String param2) {
-        EditKelas fragment = new EditKelas();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit_kelas, container, false);
+        binding = KelasPetugasBinding.inflate(inflater, container, false);
+        return binding;
     }
 }
--- app/src/main/java/com/lleans/spp_kelompok_2/ui/main/petugas/siswa/EditSiswa.java
+++ app/src/main/java/com/lleans/spp_kelompok_2/ui/main/petugas/siswa/EditSiswa.java
@@ -61,6 +61,6 @@ public class EditSiswa extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit_siswa, container, false);
+        return inflater.inflate(R.layout.petugas_edit_siswa, container, false);
     }
 }
