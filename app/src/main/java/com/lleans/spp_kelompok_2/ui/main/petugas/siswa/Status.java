0b68f9d43f3d9c9179d7ac45d5b3bb058e371b7a
salsabiladiah7 <salsabila_diah_28rpl@student.smktelkom-mlg.sch.id>
2022-01-25 09:47:41 +0700

    binding btn edit siswa

--- app/src/main/java/com/lleans/spp_kelompok_2/ui/main/petugas/siswa/Status.java
+++ app/src/main/java/com/lleans/spp_kelompok_2/ui/main/petugas/siswa/Status.java
@@ -1,15 +1,17 @@
 package com.lleans.spp_kelompok_2.ui.main.petugas.siswa;
 
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import com.lleans.spp_kelompok_2.R;
 import com.lleans.spp_kelompok_2.databinding.StatusSiswaBinding;
 
 public class Status extends Fragment {
@@ -23,6 +25,8 @@ public class Status extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        final NavController nav = Navigation.findNavController(view);
+        binding.btnEdit.setOnClickListener(v -> nav.navigate(R.id.action_statussiswa_petugas_to_editSiswa));
     }
 
     @Override
